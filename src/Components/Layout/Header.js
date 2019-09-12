7fd5dda0b4e40b520885de11df911713d4a7d71b
Edgardo Ainol Rivera <edgardo.ainol@CRECIC.CL>
2019-09-12 11:25:20 -0300

    first commit

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
+export default (props) =>
+<AppBar position="static">
+    <Toolbar>
+        <Typography variant="h6" color="inherit" gutterBottom>
+            Exercise Database
+        </Typography>
+    </Toolbar>
+</AppBar>
+
+
