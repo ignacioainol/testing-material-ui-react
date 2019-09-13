b5269e5fd5fe311781f64115dcfdd789fe2d7cae
Edgardo Ainol Rivera <edgardo.ainol@CRECIC.CL>
2019-09-13 11:23:06 -0300

    Creando panel derecho e izquierdo y mostrando listado de exercises

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import { Grid } from '@material-ui/core';
-import LeftPane from './LeftPane';
-import RightPane from './RightPane';
+import React, { Fragment } from 'react'
+import { Grid, Paper, Typography } from '@material-ui/core';
+import {List, ListItem, ListItemText} from '@material-ui/core';
 
 const styles = {
-    Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
+    Paper: { padding: 20, marginTop: 10, marginBottom: 10 , height: 500, overflowY: 'auto'}
 }
 
-export default (props) =>
-<Grid container sm={12}>
-    <Grid item sm>
-        <LeftPane styles={styles}/>
-    </Grid>
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+  }
 
-    <Grid item sm>
-        <RightPane styles={styles}/>
+export default ({ exercises }) =>
+    <Grid container item={true}>
+        <Grid item sm >
+            <Paper style={styles.Paper}>
+                {exercises.map(([group, exercises]) =>
+                    <Fragment>
+                        <Typography
+                            variant="subtitle1"
+                            style={{ textTransform: 'capitalize' }}
+                        >
+                            {group}
+                        </Typography>
+                        <List component="ul" aria-label="secondary mailbox folders">
+                            {exercises.map(exercise => 
+                                <ListItem button>
+                                    <ListItemText primary={exercise.title} />
+                                </ListItem>
+                            )}
+                        </List>
+                    </Fragment>
+                )}
+            </Paper>
+        </Grid>
+
+        <Grid item sm>
+            <Paper style={styles.Paper}>
+                <Typography
+                    variant="h5"
+                >
+                    Welcome !
+                </Typography>
+
+                <Typography
+                    variant="subtitle1"
+                    style={{marginTop: 20}}
+                >
+                    Please select an exercise from de list of the left.
+                </Typography>
+        </Paper>
+        </Grid>
     </Grid>
-</Grid>
---
+++ src/store.js
@@ -0,0 +1,48 @@
+export const muscles = [
+    "shoulders","chest","arms","back","legs"
+]
+
+export const exercises = [
+    {
+        id:'overhead-press',
+        title:'Overhead Press',
+        description: 'Delts exercises',
+        muscles:'shoulders'
+    },
+    {
+        id:'dips',
+        title:'Dips',
+        description: 'Triceps exercises',
+        muscles:'arms'
+    },
+    {
+        id:'barbell-curls',
+        title:'Barbell Curls',
+        description: 'Biceps Exercises',
+        muscles:'arms'
+    },
+    {
+        id:'bench-press',
+        title:'Bench Press',
+        description: 'Chest Exercise',
+        muscles:'chest'
+    },
+    {
+        id:'pull-ups',
+        title:'Pull Ups',
+        description: 'Back and biceps exercises',
+        muscles:'back'
+    },
+    {
+        id:'deadlifts',
+        title:'Deadlifts',
+        description: 'Back and legs exercises',
+        muscles:'back'
+    },
+    {
+        id:'squats',
+        title:'Squats',
+        description: 'Legs exercises',
+        muscles:'legs'
+    }
+]
